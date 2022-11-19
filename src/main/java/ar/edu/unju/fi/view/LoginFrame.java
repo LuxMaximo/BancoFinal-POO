6146c79211f1e857bc51f9d58874ff01341718f3
Franco <francodanielj@gmail.com>
2022-11-18 22:19:18 -0300

    agregado la clase LoginPresenter y terminado la vista del LoginFrame

--- src/main/java/ar/edu/unju/fi/view/LoginFrame.java
+++ src/main/java/ar/edu/unju/fi/view/LoginFrame.java
@@ -5,10 +5,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
 
-public class LoginFrame extends JFrame {
+public class LoginFrame extends JFrame{
 
 	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JPasswordField txtPassword;
 
 	/**
 	 * Launch the application.
@@ -30,12 +38,39 @@ public class LoginFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginFrame() {
+		setTitle("Ingreso Usuario");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 346, 190);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsuario.setBounds(33, 11, 78, 42);
+		contentPane.add(lblUsuario);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(106, 21, 140, 25);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Contraseña");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPassword.setBounds(29, 67, 76, 26);
+		contentPane.add(lblPassword);
+		
+		txtPassword = new JPasswordField();
+		txtPassword.setColumns(10);
+		txtPassword.setBounds(107, 68, 138, 26);
+		contentPane.add(txtPassword);
+		
+		JButton btnIngresar = new JButton("Ingresar");
+		btnIngresar.setBounds(101, 109, 104, 31);
+		contentPane.add(btnIngresar);
 	}
-
 }
