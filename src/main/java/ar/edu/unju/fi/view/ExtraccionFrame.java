c7066c7e55aa32ab3227c952047719efdc53b6d1
Franco <fra92nk@gmail.com>
2022-11-23 09:39:49 -0300

    cambios en extraccion frame

--- src/main/java/ar/edu/unju/fi/view/ExtraccionFrame.java
+++ src/main/java/ar/edu/unju/fi/view/ExtraccionFrame.java
@@ -16,6 +16,7 @@ public class ExtraccionFrame extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -38,7 +39,7 @@ public class ExtraccionFrame extends JFrame {
 	 */
 	public ExtraccionFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 351, 537);
+		setBounds(100, 100, 312, 469);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -46,26 +47,35 @@ public class ExtraccionFrame extends JFrame {
 contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("");
-		Image img = new ImageIcon(this.getClass().getResource("/img/cash.png")).getImage();
+		Image img = new ImageIcon(this.getClass().getResource("/img/plata.png")).getImage();
 		lblNewLabel.setIcon(new ImageIcon(img));
-		lblNewLabel.setBounds(47, 30, 329, 311);
+		lblNewLabel.setBounds(79, 28, 132, 110);
 		contentPane.add(lblNewLabel);
 		
 		JButton btnNewButton = new JButton("Extraer");
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btnNewButton.setBounds(107, 416, 111, 54);
+		btnNewButton.setBounds(89, 347, 111, 54);
 		contentPane.add(btnNewButton);
 		
-		JLabel lblNewLabel_1 = new JLabel("Monto $");
+		JLabel lblNewLabel_1 = new JLabel("Importe $");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel_1.setBounds(53, 346, 79, 38);
+		lblNewLabel_1.setBounds(23, 276, 110, 38);
 		contentPane.add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(142, 352, 116, 32);
+		textField.setBounds(130, 282, 116, 32);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
+		JLabel lblNewLabel_1_1 = new JLabel("Saldo $");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblNewLabel_1_1.setBounds(23, 211, 110, 38);
+		contentPane.add(lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(130, 211, 116, 32);
+		contentPane.add(textField_1);
+		
 	}
-
 }
